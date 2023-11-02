92aa5937e9aa767226109a17445eed372572c065
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2023-11-01 21:02:24 -0300

    Fixing methods to accept search id

--- biblioteca.js
+++ biblioteca.js
@@ -25,15 +25,19 @@ class Library {
     }
 
     removeBookById(id) {        
-        this.books.splice(id, 1);        
+      const index = this.books.findIndex(book => book.id == id);
+      if (index > 0) {
+          this.books.splice(index, 1);
+      }        
     }
     
     getBookById(id) {
-        return this.books[id];
+      const book = this.books.find(book => book.id == id);  
+      return book;
     }
     
     updateBookById(id, info) {
-        const book = this.getBookById(id);
+        const book = this.books.find(book => book.id == id);
         if (book) {
           if (info.title) {
             book.title = info.title;
@@ -46,42 +50,41 @@ class Library {
           }
           return book;
         }
-        return null;
       }
     }
 
 const library = new Library();
     
-library.addBook({
+console.log(library.addBook({
     title: "Book 1",
     description: "Book 1 description",
     author: "Book 1 author",
-});
+}));
     
-library.addBook({
+console.log(library.addBook({
     title: "Book 2",
     description: "Book 2 description",
     author: "Book 2 author",
-});
+}));
 
-library.addBook({
+console.log(library.addBook({
   title: "Book 3",
   description: "Book 3 description",
   author: "Book 3 author",
-});
+}));
     
 console.log(library.getBooks());
     
-library.updateBookById(0, {
+console.log(library.updateBookById("1", {
       title: "New title",
       description: "New description",
       author: "New author",
-});
+}));
     
 console.log(library.getBooks());
 
-console.log(library.getBookById(1));
+console.log(library.getBookById("1"));
 
-library.removeBookById(1);
+library.removeBookById("2");
 
 console.log(library.getBooks());
